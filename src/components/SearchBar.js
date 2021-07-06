6c65a632664106dc10ba37e4852a476c83256e10
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-07-06 17:06:17 -0400

    first-commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text"/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
